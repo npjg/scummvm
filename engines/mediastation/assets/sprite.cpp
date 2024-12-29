ef3833aeff47ad1c61fb5a2693b7b20afad810e7
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2024-12-29 15:53:15 -0500

    MEDIASTATION: Add initial engine prototype that can play opening movies of some titles.
    This is very incomplete, as rendering and sound playback are not handled well at all yet. However, most of the data structures in this engine are handled.

---
+++ engines/mediastation/assets/sprite.cpp
@@ -0,0 +1,246 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "mediastation/datum.h"
+#include "mediastation/assets/sprite.h"
+#include "mediastation/debugchannels.h"
+#include "mediastation/mediastation.h"
+
+namespace MediaStation {
+
+SpriteFrameHeader::SpriteFrameHeader(Chunk &chunk) : BitmapHeader(chunk) {
+	_index = Datum(chunk).u.i;
+	debugC(5, kDebugLoading, "SpriteFrameHeader::SpriteFrameHeader(): _index = 0x%x (@0x%llx)", _index, chunk.pos());
+	_boundingBox = Datum(chunk, DatumType::POINT_2).u.point;
+	debugC(5, kDebugLoading, "SpriteFrameHeader::SpriteFrameHeader(): _boundingBox (@0x%llx)", chunk.pos());
+}
+
+SpriteFrameHeader::~SpriteFrameHeader() {
+	delete _boundingBox;
+	_boundingBox = nullptr;
+}
+
+SpriteFrame::SpriteFrame(Chunk &chunk, SpriteFrameHeader *header) : Bitmap(chunk, header) {
+	_bitmapHeader = header;
+}
+
+SpriteFrame::~SpriteFrame() {
+	delete _bitmapHeader;
+	_bitmapHeader = nullptr;
+}
+
+uint32 SpriteFrame::left() {
+	return _bitmapHeader->_boundingBox->x;
+}
+
+uint32 SpriteFrame::top() {
+	return _bitmapHeader->_boundingBox->y;
+}
+
+Common::Point SpriteFrame::topLeft() {
+	return Common::Point(left(), top());
+}
+
+Common::Rect SpriteFrame::boundingBox() {
+	return Common::Rect(topLeft(), width(), height());
+}
+
+uint32 SpriteFrame::index() {
+	return _bitmapHeader->_index;
+}
+
+Sprite::~Sprite() {
+	for (SpriteFrame *frame : _frames) {
+		delete frame;
+	}
+}
+
+Operand Sprite::callMethod(BuiltInMethod methodId, Common::Array<Operand> &args) {
+	switch (methodId) {
+	case BuiltInMethod::spatialShow: {
+		assert(args.size() == 0);
+		spatialShow();
+		return Operand();
+	}
+
+	case BuiltInMethod::timePlay: {
+		assert(args.size() == 0);
+		timePlay();
+		return Operand();
+	}
+
+	case BuiltInMethod::movieReset: {
+		assert(args.size() == 0);
+		debugC(5, kDebugScript, "Sprite::movieReset(): Sprite reset");
+		_isPlaying = false;
+		_startTime = 0;
+		_lastProcessedTime = 0;
+		spatialShow();
+		// TODO: This won't add back to the playing assets array!
+		return Operand();
+	}
+
+	default: {
+		error("Got unimplemented method ID %d", methodId);
+	}
+	}
+}
+
+void Sprite::spatialShow() {
+	debugC(5, kDebugScript, "Sprite::spatialShow(): Sprite now showing");
+	_isPlaying = true;
+	_isSpatialShowOnly = true;
+	g_engine->addPlayingAsset(this);
+}
+
+void Sprite::timePlay() {
+	//if (_isPlaying) {
+	//    error("Sprite::timePlay(): Attempted to play a sprite that is already playing");
+	//    return;
+	//}
+
+	// SET ANIMATION VARIABLES.
+	debugC(5, kDebugScript, "Sprite::timePlay(): Sprite playback started");
+	_isPlaying = true;
+	_isSpatialShowOnly = false;
+	_startTime = g_system->getMillis();
+	_lastProcessedTime = 0;
+	g_engine->addPlayingAsset(this);
+
+	// GET THE DURATION OF THE SPRITE.
+	if (_header->_frameRate == 0) {
+		_header->_frameRate = 10;
+	}
+	_duration = ((double)_frames.size() / _header->_frameRate) * 1000;
+
+	// RUN THE MOVIE START EVENT HANDLER.
+	EventHandler *startEvent = _header->_eventHandlers.getValOrDefault(EventHandler::Type::MovieBegin);
+	if (startEvent != nullptr) {
+		debugC(5, kDebugScript, "Sprite::timePlay(): Executing start event handler");
+		startEvent->execute(_header->_id);
+	} else {
+		debugC(5, kDebugScript, "Sprite::timePlay(): No start event handler");
+	}
+}
+
+void Sprite::process() {
+	debugC(5, kDebugGraphics, "Sprite %d: Redrawing", _header->_id);
+	if (_isSpatialShowOnly) { // This is really isSpatialShow
+		drawFirstFrame();
+	} else {
+		drawNextFrame();
+	}
+	// Sprites arenʻt showing because weʻre not respecting z-indices, and the movie is being rendered AFTER the
+	// sprite, so it isnʻt being shown!
+
+	// TODO: I don't think sprites support time-based event handlers. Because we
+	// have a separate timer for restarting the sprite when it expires.
+}
+
+void Sprite::readChunk(Chunk &chunk) {
+	// Reads one frame from the sprite.
+	debugC(5, kDebugLoading, "Sprite::readFrame(): Reading sprite frame (@0x%llx)", chunk.pos());
+	SpriteFrameHeader *header = new SpriteFrameHeader(chunk);
+	SpriteFrame *frame = new SpriteFrame(chunk, header);
+	_frames.push_back(frame);
+
+	Common::sort(_frames.begin(), _frames.end(), [](SpriteFrame * a, SpriteFrame * b) {
+		return a->index() < b->index();
+	});
+}
+
+void Sprite::drawFirstFrame() {
+	// GET THE FIRST FRAME.
+	SpriteFrame *firstFrame = _frames[0];
+	for (SpriteFrame *frame : _frames) {
+		if (frame->index() < firstFrame->index()) {
+			firstFrame = frame;
+		}
+	}
+
+	SpriteFrame *frame = firstFrame;
+	uint frameLeft = frame->left() + _header->_boundingBox->left;
+	uint frameTop = frame->top() + _header->_boundingBox->top;
+	debugC(7, kDebugGraphics, "SPRITE: Drawing still frame %d (%d x %d) @ (%d, %d)", frame->index(), frame->width(), frame->height(), frameLeft, frameTop);
+	g_engine->_screen->transBlitFrom(frame->surface, Common::Point(frameLeft, frameTop), 0, false);
+}
+
+bool Sprite::drawNextFrame() {
+	// DETERMINE WHICH FRAMES NEED TO BE DRAWN.
+	uint currentTime = g_system->getMillis();
+	uint movieTime = currentTime - _startTime;
+	debugC(8, kDebugGraphics, "Sprite::drawNextFrame(): Starting frame blitting (sprite time: %d)", movieTime);
+	if (movieTime > _duration) {
+		// RESET ANIMATION VARIABLES.
+		_isPlaying = false;
+		_startTime = 0;
+		_lastProcessedTime = 0;
+
+		// RUN THE SPRITE END EVENT HANDLER.
+		EventHandler *endEvent = _header->_eventHandlers.getValOrDefault(EventHandler::Type::MovieEnd);
+		if (endEvent != nullptr) {
+			debugC(5, kDebugScript, "Sprite::drawNextFrame(): Executing end event handler");
+			endEvent->execute(_header->_id);
+		} else {
+			debugC(5, kDebugScript, "Sprite::drawNextFrame: No end event handler");
+		}
+		return false;
+	}
+
+	uint frameIndex = 0;
+	Common::Array<SpriteFrame *> framesToDraw;
+	for (SpriteFrame *frame : _frames) {
+		// Unlike movies, sprite frames don't have individual durations. They
+		// are guaranteed to be multiples of the given frame rate.
+		uint frameStartInMilliseconds = frameIndex * _header->_frameRate * 10;
+		uint frameEndInMilliseconds = (frameIndex + 1) * _header->_frameRate * 10;
+		// Unlike movies, sprite frame coordinates are relative to the sprite
+		// bounding box.
+		uint frameLeft = frame->left() + _header->_boundingBox->left;
+		uint frameTop = frame->top() + _header->_boundingBox->top;
+
+		bool isAfterStart = (_startTime + frameStartInMilliseconds) <= currentTime;
+		bool isBeforeEnd = (_startTime + frameEndInMilliseconds) >= currentTime;
+		if (!isAfterStart || (isAfterStart && !isBeforeEnd)) {
+			frameIndex++;
+			continue;
+		}
+		debugC(7, kDebugGraphics, "SPRITE: (time: %d ms) Drawing frame %d (%d x %d) @ (%d, %d); start: %d ms, end: %d ms", movieTime, frame->index(), frame->width(), frame->height(), frameLeft, frameTop, frameStartInMilliseconds, frameEndInMilliseconds);
+		framesToDraw.push_back(frame);
+		frameIndex++;
+	}
+
+	// BLIT THE FRAMES.
+	for (SpriteFrame *frame : framesToDraw) {
+		uint frameLeft = frame->left() + _header->_boundingBox->left;
+		uint frameTop = frame->top() + _header->_boundingBox->top;
+		debugC(7, kDebugGraphics, "SPRITE: (time: %d ms) Drawing frame %d (%d x %d) @ (%d, %d)", movieTime, frame->index(), frame->width(), frame->height(), frameLeft, frameTop);
+		g_engine->_screen->transBlitFrom(frame->surface, Common::Point(frameLeft, frameTop), 0, false);
+	}
+	// The main game loop takes care of updating the screen.
+
+	uint frameBlitEnd = g_system->getMillis() - _startTime;
+	uint elapsedTime = frameBlitEnd - movieTime;
+	debugC(8, kDebugGraphics, "Sprite::drawNextFrame(): Finished frame blitting in %d ms (current sprite time: %d ms)", elapsedTime, frameBlitEnd);
+	return true;
+}
+
+} // End of namespace MediaStation
---
+++ engines/mediastation/mediastation.cpp
@@ -0,0 +1,244 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "graphics/framelimiter.h"
+#include "common/scummsys.h"
+#include "common/config-manager.h"
+#include "common/debug-channels.h"
+#include "common/events.h"
+#include "common/system.h"
+#include "engines/util.h"
+#include "graphics/paletteman.h"
+
+#include "mediastation/mediastation.h"
+#include "mediastation/debugchannels.h"
+#include "mediastation/detection.h"
+#include "mediastation/boot.h"
+#include "mediastation/context.h"
+#include "mediastation/asset.h"
+#include "mediastation/assets/movie.h"
+
+namespace MediaStation {
+
+MediaStationEngine *g_engine;
+
+MediaStationEngine::MediaStationEngine(OSystem *syst, const ADGameDescription *gameDesc) : Engine(syst),
+	_gameDescription(gameDesc),
+	_randomSource("MediaStation"),
+	_boot(nullptr) {
+	g_engine = this;
+	_mixer = g_system->getMixer();
+}
+
+MediaStationEngine::~MediaStationEngine() {
+	_mixer = nullptr;
+
+	delete _screen;
+	_screen = nullptr;
+
+	delete _boot;
+	_boot = nullptr;
+
+	for (auto it = _assets.begin(); it != _assets.end(); ++it) {
+		delete it->_value;
+	}
+	_assets.clear();
+	_assetsByChunkReference.clear();
+
+	for (auto it = _functions.begin(); it != _functions.end(); ++it) {
+		delete it->_value;
+	}
+	_functions.clear();
+
+	for (auto it = _variables.begin(); it != _variables.end(); ++it) {
+		delete it->_value;
+	}
+	_variables.clear();
+}
+
+uint32 MediaStationEngine::getFeatures() const {
+	return _gameDescription->flags;
+}
+
+Common::String MediaStationEngine::getGameId() const {
+	return _gameDescription->gameId;
+}
+
+bool MediaStationEngine::isFirstGenerationEngine() {
+	if (_boot == nullptr) {
+		error("Attempted to get engine version before BOOT.STM was read");
+	} else {
+		return (_boot->_versionInfo == nullptr);
+	}
+}
+
+Common::Error MediaStationEngine::run() {
+	// INITIALIZE SUBSYSTEMS.
+	// All Media Station games run at 640x480.
+	initGraphics(640, 480);
+	_screen = new Graphics::Screen();
+	_screen->fillRect(Common::Rect(640, 480), 255);
+
+	// LOAD BOOT.STM.
+	Common::Path bootStmFilepath = Common::Path("BOOT.STM");
+	_boot = new Boot(bootStmFilepath);
+
+	// LOAD THE ROOT CONTEXT.
+	// This is because we might have assets that always need to be loaded.
+	Context *root = nullptr;
+	uint32 rootContextId = _boot->getRootContextId();
+	if (rootContextId != 0) {
+		root = loadContext(rootContextId);
+	} else {
+		warning("MediaStation::run(): Title has no root context");
+	}
+
+	Context *activeScreen = loadContext(_boot->_entryContextId);
+	if (activeScreen->_screenAsset != nullptr) {
+		// GET THE PALETTE.
+		setPaletteFromHeader(activeScreen->_screenAsset);
+
+		// PROCESS THE OPENING EVENT HANDLER.
+		EventHandler *entryEvent = activeScreen->_screenAsset->_eventHandlers.getValOrDefault(EventHandler::Type::Entry);
+		if (entryEvent != nullptr) {
+			debugC(5, kDebugScript, "Executing context entry event handler");
+			entryEvent->execute(activeScreen->_screenAsset->_id);
+		} else {
+			debugC(5, kDebugScript, "No context entry event handler");
+		}
+	}
+
+	uint32 currentTime = g_system->getMillis();
+	while (true) {
+		// PROCESS EVENTS.
+		Common::ErrorCode status = processEvents();
+		if (status == Common::kNoError) {
+			return status;
+		}
+
+		// PROCESS ANY ASSETS CURRENTLY PLAYING.
+		// First, they all need to be sorted by z-coordinate.
+		Common::sort(_assetsPlaying.begin(), _assetsPlaying.end(), [](Asset * a, Asset * b) {
+			return a->zIndex() > b->zIndex();
+		});
+		for (auto it = _assetsPlaying.begin(); it != _assetsPlaying.end();) {
+			(*it)->process();
+			if (!(*it)->isPlaying()) {
+				it = _assetsPlaying.erase(it);
+			} else {
+				++it;
+			}
+		}
+
+		g_engine->_screen->update();
+		g_system->delayMillis(10);
+	}
+
+	return Common::kNoError;
+}
+
+Common::ErrorCode MediaStationEngine::processEvents() {
+	while (g_system->getEventManager()->pollEvent(e)) {
+		debugC(9, kDebugEvents, "\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		debugC(9, kDebugEvents, "@@@@   Processing events");
+		debugC(9, kDebugEvents, "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+
+		switch (e.type) {
+		case Common::EVENT_QUIT: {
+			error("Quitting");
+		}
+
+		case Common::EVENT_KEYDOWN: {
+			error("Quitting");
+		}
+
+		case Common::EVENT_LBUTTONDOWN: {
+			error("Quitting");
+		}
+
+		default: {
+			break;
+		}
+		}
+	}
+	return Common::kUserCanceled; //Common::kNoError;
+}
+
+Context *MediaStationEngine::loadContext(uint32 contextId) {
+	if (_boot == nullptr) {
+		error("Cannot load contexts before BOOT.STM is read");
+	}
+
+	// GET THE FILE ID.
+	SubfileDeclaration *subfileDeclaration = _boot->_subfileDeclarations.getValOrDefault(contextId);
+	if (subfileDeclaration == nullptr) {
+		warning("MediaStationEngine::loadContext(): Couldn't find subfile declaration with ID 0x%x", contextId);
+		return nullptr;
+	}
+	// The subfile declarations have other assets too, so we need to make sure
+	if (subfileDeclaration->_startOffsetInFile != 16) {
+		warning("MediaStationEngine::loadContext(): Requested ID wasn't for a context.");
+		return nullptr;
+	}
+	uint32 fileId = subfileDeclaration->_fileId;
+
+	// GET THE FILENAME.
+	FileDeclaration *fileDeclaration = _boot->_fileDeclarations.getValOrDefault(fileId);
+	if (fileDeclaration == nullptr) {
+		warning("MediaStationEngine::loadContext(): Couldn't find file declaration with ID 0x%x", fileId);
+		return nullptr;
+	}
+	Common::String *fileName = fileDeclaration->_name;
+
+	// LOAD THE CONTEXT.
+	Common::Path entryCxtFilepath = Common::Path(*fileName);
+	Context *context = new Context(entryCxtFilepath);
+
+	// SET THE VARIABLES.
+	return context;
+}
+
+void MediaStationEngine::setPalette(Asset *palette) {
+	assert(palette != nullptr);
+	setPaletteFromHeader(palette->getHeader());
+}
+
+void MediaStationEngine::setPaletteFromHeader(AssetHeader *header) {
+	assert(header != nullptr);
+	if (header->_palette != nullptr) {
+		_screen->setPalette(*header->_palette);
+	} else {
+		warning("MediaStationEngine::setPaletteFromHeader(): Asset %d does not have a palette. Current palette will be unchanged.", header->_id);
+	}
+}
+
+void MediaStationEngine::addPlayingAsset(Asset *assetToAdd) {
+	// If we're already marking the asset as played, we don't need to mark it
+	// played again.
+	for (Asset *asset : g_engine->_assetsPlaying) {
+		if (asset == assetToAdd) {
+			return;
+		}
+	}
+	g_engine->_assetsPlaying.push_back(assetToAdd);
+}
+
+} // End of namespace MediaStation
